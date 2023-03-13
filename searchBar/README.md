9c3087ccdb57159570a602258b674e651b927418
Aman Anupam <amananupamhvz@gmail.com>
2023-03-13 21:28:27 +0530

    vanilla search bar

---
+++ searchBar/README.md
@@ -0,0 +1,8 @@
+search bar
+
+1. search data based on query from the API (use debouncing) --- API response is already filtered. Other thing that we can do is get the list of all countries one time and then filter the countries using "includes"
+
+2. render the results in a list
+
+3. on click of any search item inside the list, close the list and put the selected item into the search bar (use event delegation)
+
---
+++ searchBar/index.js
@@ -0,0 +1,60 @@
+
+const fetchData = async (query) =>{
+    const response = await fetch(`https://restcountries.com/v3.1/name/${query}`)
+    console.log(response);
+    const data = await response.json();
+    return data;
+}
+
+const searchBar = document.querySelector(".searchBar");
+const container = document.querySelector(".container");
+
+let query="";
+
+const debounce = (fn, duration) => {
+    let timer = null;
+    return function (...args){ 
+        if(timer){
+            clearTimeout(timer)
+            timer = null
+        }
+        timer = setTimeout(()=>{
+            fn.apply(this,args)
+        }, duration)
+    }
+}
+
+container.addEventListener("click", (event)=>{
+    console.log(event);
+    if(event.target.matches("span")){
+        searchBar.value = `${event.target.innerHTML}`
+        container.innerHTML = ''
+    }
+})
+
+const searchCallback = async (event)=>{
+    query = event.target.value;
+    console.log(query)
+    let searchResults
+    if(query)
+        searchResults  = await fetchData(query);
+    console.log(searchResults);
+
+    if(searchResults && searchResults.length){
+        container.innerHTML = ''
+        searchResults.forEach(element => {
+            console.log(element.name)
+            let htmlStr = `
+            <div class="item">
+                <span>${element.name.official}</span>
+            </div>
+        `
+        container.insertAdjacentHTML("beforeend", htmlStr)
+        });
+    }
+    else container.innerHTML = ''
+
+}
+
+searchBar.addEventListener("input", debounce(searchCallback, 1000))
+
